fbc4dbbc07cef889f2e0f3bfd3b4ee73cc24345f
AnitaGamboa <anitagamboabaez@gmail.com>
2021-11-23 18:10:36 -0500

    Reto1 C4 preliminar

--- src/main/java/com/r1c4/mercaderia/MercaderiaApplication.java
+++ src/main/java/com/r1c4/mercaderia/MercaderiaApplication.java
@@ -1,13 +1,13 @@
-package com.rentcloud.cloud;
+package com.r1c4.mercaderia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudApplication {
+public class MercaderiaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudApplication.class, args);
+		SpringApplication.run(MercaderiaApplication.class, args);
 	}
 
 }
--- src/test/java/com/r1c4/mercaderia/MercaderiaApplicationTests.java
+++ src/test/java/com/r1c4/mercaderia/MercaderiaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.rentcloud.cloud;
+package com.r1c4.mercaderia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CloudApplicationTests {
+class MercaderiaApplicationTests {
 
 	@Test
 	void contextLoads() {
